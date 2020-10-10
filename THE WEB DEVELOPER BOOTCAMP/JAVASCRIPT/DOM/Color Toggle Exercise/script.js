c76cbc07ea8d286d84d326bc08cf9840ab18522c
quevedo88 <jlquevedo88@gmail.com>
2020-10-10 10:52:20 -0600

    Exercise about DOM

---
+++ THE WEB DEVELOPER BOOTCAMP/JAVASCRIPT/DOM/Color Toggle Exercise/script.js
@@ -0,0 +1,29 @@
+var button = document.querySelector("button");
+//var isOrange = false;
+
+/*button.addEventListener("click", function (){
+	if(isOrange){
+		document.body.style.background = "white";
+		isOrange = false;
+	} else{
+		document.body.style.background = "orange";
+		isOrange = true;
+	}
+
+	//A shortcut with isOrange true or false is doing this: isOrange = !isOrange;
+	//add some logic
+	//if white
+		//make it orange
+	//else 
+	//make it orange
+
+	
+	//document.body.style.background = "orange";
+	button.style.background = "purple";
+	//console.log("Someone clicked the button");
+});*/
+
+ //Another solution
+ button.addEventListener("click", function (){
+		document.body.classList.toggle = ("orange");
+	});
